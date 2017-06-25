54266c7e62025c3816a6987191c40f3bc0fdd97c
Samray Leung <samrayleung@gmail.com>
2017-06-25 18:14:54 +0800

    add error handling in codecs

---
+++ the_file_system/codecs/codecs_encode_error.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# author:Samray <samrayleung@gmail.com>
+
+import codecs
+import sys
+
+error_handling = sys.argv[1]
+
+text = 'français'
+
+try:
+    # Save the data, encoded as ASCII, using the error
+    # handling mode specfied on the command line.
+    with codecs.open('encode_error.txt', 'w', encoding='ascii', errors=error_handling) as f:
+        f.write(text)
+
+except UnicodeEncodeError as err:
+    print("ERROR:", err)
+else:
+    # If there was no error writing to the file
+    # show what it contains
+    with open('encode_error.txt', 'rb') as f:
+        print('File contents: {!r}'.format(f.read()))
